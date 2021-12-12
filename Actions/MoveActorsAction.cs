5a522116eaf709ed9885c4d7cbb7c3e981df310d
elijahw-byui <elijahw@byui.edu>
2021-12-12 00:48:58 -0700

    p

--- Actions/MoveActorsAction.cs
+++ Actions/MoveActorsAction.cs
@@ -8,6 +8,8 @@ namespace cse210_RH2_csharp.Scripting
     public class MoveActorsAction : Action
     {
         PhysicsService _physicsService = new PhysicsService();
+        private bool first = true;
+        private int counter = 0;
 
         public MoveActorsAction(PhysicsService physicsService)
         {
@@ -22,9 +24,76 @@ namespace cse210_RH2_csharp.Scripting
                 {   
                     
                     acting.MoveNext();
-                    if (acting.isRobinhood())
+                    if (acting.isRichguy())
                     {
+                        Point p = acting.GetVelocity();
+                        int x = p.GetX();
+                        int y = p.GetY();
+                        if (y < 0)
+                        {
+                            bool second = !first;
+                                
+                                    if (second)
+                                    {
+                                        acting.SetImage(Constants.IMAGE_UL);   
+                                    }
+                                    else if (!second)
+                                    {
+                                        acting.SetImage(Constants.IMAGE_UR);
+                                    }
+                                
+                            counter++;                                
+                            first = second;
                         
+                        }
+                        else if (y > 0)
+                        {
+                            bool second = !first;
+                                
+                                if (second)
+                                {
+                                    acting.SetImage(Constants.IMAGE_DL);   
+                                }
+                                else if (!second)
+                                {
+                                    acting.SetImage(Constants.IMAGE_DR);
+                                }
+                                
+                                counter++;                                
+                                first = second;
+                        }
+                        else if (x < 0)
+                        {
+                            bool second = !first;
+                                
+                                if (second)
+                                {
+                                    acting.SetImage(Constants.IMAGE_LL);   
+                                }
+                                else if (!second)
+                                {
+                                    acting.SetImage(Constants.IMAGE_LR);
+                                }
+                                
+                                counter++;                                
+                                first = second;
+                        }
+                        else if (x > 0)
+                        {
+                            bool second = !first;
+                                
+                                if (second)
+                                {
+                                    acting.SetImage(Constants.IMAGE_RL);   
+                                }
+                                else if (!second)
+                                {
+                                    acting.SetImage(Constants.IMAGE_RR);
+                                }
+                            
+                                counter++;                                
+                                first = second;
+                        }
                     }
                 
                 }
